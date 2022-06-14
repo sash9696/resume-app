88c2b9bd755a2db3f81fa2e052e6d92eb931f0eb
Harshit <sahilchopra838@gmail.com>
2022-06-14 21:19:34 +0530

    made hompage and nav component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Homepage from "./components/Homepage/Homepage";
+//Portfolio Website
+
+//1.Navbar
+//2.HomePage
+//3.Tech Stack
+//4.Experience
+//5.Projects
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
+      <Homepage />
     </div>
   );
 }
---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Homepage.css";
+import githubIcon from "../../images/githubIcon.png";
+import linkedinIcon from "../../images/linkedinIcon.png";
+
+function Homepage() {
+  return (
+    <div className="homepage_container">
+      <div className="homepage_left">
+        <p className="homepage_left_h1">
+          <span className="red_color">Hi,</span> my name is
+        </p>
+        <p className="homepage_left_h2">Sahil Chopra</p>
+        <p className="homepage_left_h3">
+          Developer at <span className="red_color">Somewhere</span>{" "}
+        </p>
+        <p className="homepage_left_description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+          fringilla nec risus eu efficitur. Donec rhoncus nec ligula vel
+          accumsan. Phasellus vitae congue nibh, vel aliquam neque. Aliquam eu
+          suscipit lorem.{" "}
+        </p>
+        <div>
+          <a href="https://github.com/sash9696" target="_blank">
+            <img src={githubIcon} alt="githubIconimage" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sahil-chopra-56a63b191/"
+            target="_blank"
+          >
+            <img src={linkedinIcon} alt="linkedinIconimage" />
+          </a>
+        </div>
+        <a href="mailto:sahilchopra838@gmail.com">
+          <button className="homepage_left_button">Get in Touch</button>
+        </a>
+      </div>
+      <div className="homepage_right">
+        <img
+          src="https://i.pinimg.com/originals/e4/26/70/e426702edf874b181aced1e2fa5c6cde.gif"
+          alt="GIF"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
